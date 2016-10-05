b42988cd97327eb6ad28e4248c3d2dbaff85fc70
slayer3600 <slayer3600@hotmail.com>
2016-10-05 16:51:19 -0700

    ...

--- bookmarklets/bookmarklets/scripts/TacomaNewsTribune.js
+++ bookmarklets/bookmarklets/scripts/TacomaNewsTribune.js
@@ -1,3 +1,37 @@
-﻿(function(){
-    alert('hello');
+﻿(function () {
+
+    var v = "1.3.2";
+
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+        var done = false;
+        var script = document.createElement("script");
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function () {
+            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                done = true;
+                initMyBookmarklet();
+            }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+        initMyBookmarklet();
+    }
+
+    function initMyBookmarklet() {
+        (window.myBookmarklet = function () {
+            function getSelText() {
+                var s = '';
+                if (window.getSelection) {
+                    s = window.getSelection();
+                } else if (document.getSelection) {
+                    s = document.getSelection();
+                } else if (document.selection) {
+                    s = document.selection.createRange().text;
+                }
+                return s;
+            }
+            alert('Hello!');
+        })();
+    }
+
 })();
